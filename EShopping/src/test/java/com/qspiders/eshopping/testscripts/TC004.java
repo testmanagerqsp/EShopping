51922f1da75e25b1376379adee603240a850d7f6
Hemanth Raj <dheevj@gmail.com>
2021-04-16 11:29:21 +0530

    Added TC004

---
+++ EShopping/src/test/java/com/qspiders/eshopping/testscripts/TC004.java
@@ -0,0 +1,34 @@
+package com.qspiders.eshopping.testscripts;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.qspiders.eshopping.genericlibs.ExcelLibrary;
+import com.qspiders.eshopping.pages.DressesPage;
+import com.qspiders.eshopping.pages.OrderDetailsPage;
+import com.qspiders.eshopping.pages.ProductDetailsPage;
+
+public class TC004 extends BaseTest {
+	
+	@DataProvider
+	public String[][] getData(){
+		return ExcelLibrary.getMultipleData("TC003");
+	}
+	
+	@Test(dataProvider="getData",description="Add the Product to ODP and verify it is displayed in ODP")
+	public void testProductInODP(String menuLinkName,
+								 String productId,
+								 String quantity,
+								 String increase,
+								 String size,
+								 String colorName) {
+		productId=productId.split("\\.")[0];
+		DressesPage dressPage=(DressesPage)homePage.clickOnMenu(menuLinkName);
+		ProductDetailsPage productDetailsPage = dressPage.clickOnProduct(productId);
+		int qty = (int) Double.parseDouble(quantity);
+		boolean incrs = Boolean.parseBoolean(increase);
+		OrderDetailsPage orderDetailsPage = productDetailsPage.addItemToKart(qty, incrs, size, colorName);
+		Assert.assertTrue(orderDetailsPage.isProductDisplayed(productId), productId+" is Not Displayed");
+	}
+}
